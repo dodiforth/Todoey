f4109f6aad663c187da4bda6bbbaf5e221cc25f2
Dowon Kim <dowoniscool@yahoo.com>
2023-08-31 11:17:30 +0200

    Add a button allows addin NewItems - creating TextField inside UIAlert

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -8,13 +8,23 @@
 import UIKit
 
 class TodoListViewController: UITableViewController {
-
+    
     //Hard Code Array items on startup view
-    let itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+    var itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setupNavBarAndTitleColor()
+    }
+    
+    //⚠️ NavBar tintColor & titleColor storyboard error fix :
+    func setupNavBarAndTitleColor() {
+        let appearance = UINavigationBarAppearance()
+        appearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+        appearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+        appearance.backgroundColor = UIColor.systemBlue
+        navigationItem.standardAppearance = appearance
+        navigationItem.scrollEdgeAppearance = appearance
     }
     
     // MARK: - TableView DataSource Methods
@@ -42,18 +52,29 @@ class TodoListViewController: UITableViewController {
         }
         tableView.deselectRow(at: indexPath, animated: true)
     }
-
-
-
+    
+    // MARK: - Add New Items
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            //What will happen once the user clicks the "Add Item" button on our UIAlert
+            self.itemArray.append(textField.text!)
+            self.tableView.reloadData()
+        }
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+            
+            print("NOW")
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
 }
 
 
 
-/* NavBar tintColor & titleColor storyboard error fix :
- let appearance = UINavigationBarAppearance()
- appearance.titleTextAttributes = [.foregroundColor: UIColor.white]
- appearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
- appearance.backgroundColor = UIColor.systemBlue
- navigationItem.standardAppearance = appearance
- navigationItem.scrollEdgeAppearance = appearance
- */
