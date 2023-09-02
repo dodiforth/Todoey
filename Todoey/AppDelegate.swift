d814cd2d3c0c62f74037a48e82f71f59f8950e47
Dowon Kim <dowoniscool@yahoo.com>
2023-09-02 02:13:45 +0200

    Deleted CoreData codes from App and Scene Delegate

--- Todoey/AppDelegate.swift
+++ Todoey/AppDelegate.swift
@@ -14,26 +14,24 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
     
-    func applicationWillTerminate(_ application: UIApplication) {
-        self.saveContext()
-    }
+    
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        print(Realm.Configuration.defaultConfiguration.fileURL)
+//        print(Realm.Configuration.defaultConfiguration.fileURL)
         
         // 🟣 Realm !
         do {
-            let realm = try Realm()
+            _ = try Realm()
         } catch {
             print("Error initialising new realm, \(error)")
         }
         
         return true
     }
-
+    
     // MARK: UISceneSession Lifecycle
-
+    
     // func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
     
     // // Called when a new scene session is being created.
@@ -55,34 +53,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     
     // }
-
-    // MARK: - Core Data stack
-
-    lazy var persistentContainer: NSPersistentContainer = {
     
-        let container = NSPersistentContainer(name: "DataModel")
-        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-            
-            if let error = error as NSError? {
-                fatalError("Unresolved error \(error), \(error.userInfo)")
-            }
-        })
-        return container
-        
-    }()
     
-    // MARK: - Core Data Saving support
-    
-    func saveContext () {
-        
-        let context = persistentContainer.viewContext
-        if context.hasChanges {
-            do {
-                try context.save()
-            } catch {
-                let nserror = error as NSError
-                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-            }
-        }
-    }
 }
